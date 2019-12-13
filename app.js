b7b01c2ddb5634a4dcb79bdf05a2b0ce5f7b8ec4
ravitupkar <ravitupkar01@gmail.com>
2019-12-13 20:31:26 +0530

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// var authRouter = require('./routes/auth');
+
 
 var app = express();
 
+
+mongoose.connect(process.env.MONGODBURI, {useNewUrlParser: true, useUnifiedTopology: true})
+// .then(result =>{
+//     console.log('Database connect successfull');
+// })
+// .catch(err =>{
+//   console.log('Database not connected');
+// });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
+app.use(bodyparser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
